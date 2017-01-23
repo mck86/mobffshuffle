b78814d642b1bbbc2aa4b0a7995fd008ebf9d7cd
Mike <mike.c.korn@gmail.com>
2017-01-23 16:42:44 -0500

    misc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,29 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-	concat: {
-	options: {
-	  separator: ';',
-	},
-	dist: {
-	  src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
-	  dest: 'dist/built.js',
-	},
-	},
-	});
+    grunt.initConfig({
+        concat: {
+            scripts: {
+                src: ['src/scripts/main.js'],
+                dest: 'dist/scripts/main.js',
+            },
+            styles: {
+                src: ['src/styles/main.css'],
+                dest: 'dist/styles/main.css',
+            },
+        },
+        watch: {
+            scripts: {
+                files: ['src/scripts/*.js'],
+                tasks: ['concat:scripts'],
+            },
+            styles: {
+                files: ['src/styles/*.css'],
+                tasks: ['concat:styles'],
+            },
+        },
+    });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['concat','watch']);
 };
